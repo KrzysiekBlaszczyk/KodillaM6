d9234c4a376bb9ed4bcab5ebd288208dc93a7903
Krzysiek Błaszczyk <krzysztofblaszczyk@Krzysztofs-MacBook-Pro.local>
2022-05-19 10:45:06 +0200

    6.4 Implemet methods

--- kodilla-testing/src/main/java/com/kodilla/testing/shape/ShapeCollector.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/shape/ShapeCollector.java
@@ -1,26 +1,32 @@
 package com.kodilla.testing.shape;
 
-import com.kodilla.testing.forum.ForumPost;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class ShapeCollector {
     private List<Shape> list = new ArrayList<>();
-    public void addFigure(Shape shape){
 
+    public void addFigure(Shape shape) {
+        list.add(shape);
     }
-    public boolean removeFigure(Shape shape){
 
-        return true;
+    public boolean removeFigure(Shape shape) {
+        return list.remove(shape);
     }
-    public Shape getFigure(int n){
-       return null;
+
+    public Shape getFigure(int n) {
+        if (n < list.size() && n >=0) {
+            return list.get(n);
+        } else {
+            return null;
+        }
     }
-    public Shape showFigures(){
-        return null;
+
+    public String showFigures() {
+        return list.toString();
     }
+
     public int getListQuantity() {
-        return 10;
+        return list.size();
     }
 }
